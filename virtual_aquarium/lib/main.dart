44bac91f3fdd358e1b4975f296ef82d724049a41
Bang Pham <bangpham@Bangs-MBP.attlocal.net>
2024-10-23 11:53:15 -0400

    add: user interface for aquarium container & settings

--- virtual_aquarium/lib/main.dart
+++ virtual_aquarium/lib/main.dart
@@ -7,119 +7,118 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Virtual Aquarium',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AquariumScreen(),
+     
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+//ui for aquarium
+class AquariumScreen extends StatefulWidget {
+  const AquariumScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AquariumScreenState createState() => _AquariumScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _AquariumScreenState extends State<AquariumScreen> {
+  //default color & speed
+  Color color = Colors.blue;
+  double speed = 1.0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Virtual Aquarium 🐟' , style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold), ),
+        backgroundColor: Colors.blue,
+        
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.start,
+          children:
+          [
+            //300 x 300 container
+          Padding(padding: const EdgeInsets.only(top: 16), child:   Container(
+              width: 300,
+              height: 300,
+              color: Colors.blue,
+              child: const Center(
+                child: Text('Aquarium Container', ),
+              ),
+            ),),
+            //slider to control speed
+           
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Speed: ', style: TextStyle(fontSize: 16),),
+                SliderTheme(data: const SliderThemeData(thumbColor: Colors.blue, activeTrackColor: Colors.blue), child: Slider(value: speed, min: .5, max: 5.0, onChanged: (value) {
+                  setState(() {
+                    speed = value;
+                  });
+                }),)
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+          //color picker
+        Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Color: ', style: TextStyle(fontSize: 16),),
+               DropdownButton<Color>(
+                 value: color,
+                 items: const [
+                   DropdownMenuItem<Color>(value: Colors.blue, child: Text('Blue 🔵')),
+                   DropdownMenuItem<Color>(value: Colors.green, child: Text('Green 🟢')),
+                   DropdownMenuItem<Color>(value: Colors.red, child: Text('Red 🔴')),
+                   DropdownMenuItem<Color>(value: Colors.yellow, child: Text('Yellow 🟡')),
+                 ],
+                 onChanged: (Color? newColor) {
+                   if(newColor != null) {
+                      setState(() {
+                        color = newColor;
+                      });
+                   }
+                 },
+               )
+              ],
             ),
-          ],
+
+            //add fish btn
+             Padding(padding: const EdgeInsets.only(top: 16), child: 
+            ElevatedButton(
+              onPressed: () {
+                //add fish functionality
+              },
+                style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text('Add Fish 🐟', style: TextStyle(fontSize: 16, color: Colors.white),),
+            ),),
+            //save settings btn
+             Padding(padding: const EdgeInsets.only(top: 16), child: 
+            ElevatedButton(
+              onPressed: () {
+                //save settings functionality
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: const Text('Save Settings', style: TextStyle(fontSize: 16 , color: Colors.white),),
+            ),),
+          ] 
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
